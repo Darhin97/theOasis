543999797eeaee8cf99d98131d5121fe57371be5
Derrick Ankrah <darhin97@gmail.com>
2023-08-28 04:13:54 +0000

    Add: bookings feat

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,15 +4,17 @@ import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import { useSearchParams } from "react-router-dom";
 import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
+// const Table = styled.div`
+//   border: 1px solid var(--color-grey-200);
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+//   font-size: 1.4rem;
+//   background-color: var(--color-grey-0);
+//   border-radius: 7px;
+//   overflow: hidden;
+// `;
 
 const TableHeader = styled.header`
   display: grid;
@@ -62,19 +64,22 @@ const CabinTable = () => {
   if (!cabins.length) return <Empty resourceName={"cabins"} />;
 
   return (
-    <Table role="table">
-      <TableHeader role="row">
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </TableHeader>
-      {sortedCabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
-    </Table>
+    <Menus>
+      <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={sortedCabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
   );
 };
 
