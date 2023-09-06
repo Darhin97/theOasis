a6c4ab8153c03af60ef05f42e8131364a3c3ceeb
Derrick Ankrah <darhin97@gmail.com>
2023-09-06 21:48:49 +0000

    dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import useCabins from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +13,34 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    confirmedStays,
+    stays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+
+  if (isLoading || isLoadingStays || isLoadingCabins) return <Spinner />;
+
+  // console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today's activities</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import {
+  getBookingsAfterDate,
+  getStaysAfterDate,
+} from "../../services/apiBookings";
+
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+
+  //calc for 30 or 90days ago
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  //calc confirmed stays
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+
+  return { isLoading, stays, confirmedStays, numDays };
+}
